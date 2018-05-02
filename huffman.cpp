38449eff19fe91c2478f567fa5c0f7023ddda704
Clement Liaw <cklliaw@edu.uwaterloo.ca>
2018-05-02 11:02:26 -0400

    Huffman encoding complete

--- huffman.cpp
+++ huffman.cpp
@@ -2,44 +2,78 @@
  #include <climits>
  #include <utility>
  #include <vector>
+ #include <map>
+ #include <algorithm>
  
- #define COUNT 7
+ using std::string;
+ 
+ #define COUNT 15
  
 
 class node {
 public:
     int value;
+    string symbols;
+    char code;
     node* left;
     node* right;
     node();
     node(int);
+    node(int, string);
+    node(node*, node*);
 };
+
 
 class tree {
 public:
     node* head;
     tree();
     tree(int);
+    tree(node*);
     void addElement(int);
     std::pair<bool, node*> inTree(int);
     void removeElement(int);
     std::vector<int> traverse();
     void traverseHelper(node*, std::vector<int>&);
+    void traverseApplyCode();
+    void applyCodeHelper(node*);
+    std::map<string, string> traverseGetCode();
+    void getCodeHelper(node*, string, string, std::map<string, string>&);
 };
+
 
 node::node(){
     this->value = INT_MAX;
+    this->symbols = '\0';
+    this->code = '\0';
     this->left = nullptr;
     this->right = nullptr;
 };
 
 node::node(int num) {
     this->value = num;
+    this->symbols = '\0';
+    this->code = '\0';
     this->left = nullptr;
     this->right = nullptr;    
 };
 
 
+node::node(int num, string c) {
+    this->value = num;
+    this->symbols = c;
+    this->code = '\0';
+    this->left = nullptr;
+    this->right = nullptr;    
+};
+
+node::node(node* left, node* right) {
+    this->value = left->value + right->value;
+    this->symbols = left->symbols + right ->symbols;
+    this->code = '\0';
+    this->left = left;
+    this->right = right;
+}
 tree::tree() {
     this->head = new node();
 };
@@ -48,11 +82,15 @@ tree::tree(int first) {
     this->head = new node(first);
 };
 
+tree::tree(node* first) {
+    this->head = first;
+};
+
 void tree::addElement(int num) {
     node* current_node = this->head;
     node* vanguard_node = this->head;
     
-    if(this->head->value == INT_MAX) {
+    if (this->head->value == INT_MAX) {
         this->head->value = num;
     } else {
         if (this->head->value >= num) {
@@ -62,7 +100,7 @@ void tree::addElement(int num) {
         };
         
         while(vanguard_node != nullptr) {
-            if( vanguard_node->value >= num) {
+            if (vanguard_node->value >= num) {
                 current_node = vanguard_node;
                 vanguard_node = vanguard_node->left;
             } else {
@@ -98,7 +136,7 @@ std::pair<bool, node*> tree::inTree(int num) {
     }
     
     while(vanguard_node != nullptr) {
-        if(vanguard_node->value > num) {
+        if (vanguard_node->value > num) {
             current_node = vanguard_node;
             vanguard_node = vanguard_node->left;
         } else if (vanguard_node->value < num) {
@@ -221,6 +259,41 @@ void tree::traverseHelper(node* next, std::vector<int>& order) {
     };
 };
 
+void tree::traverseApplyCode() {
+    applyCodeHelper(this->head);
+}
+
+void tree::applyCodeHelper(node* next) {
+    if (next != nullptr) {
+        if (next->left != nullptr) {
+            next->left->code = '0';
+        }
+        applyCodeHelper(next->left);
+        if (next->right != nullptr) {
+            next->right->code = '1';
+        }
+        applyCodeHelper(next->right);
+    }
+}
+
+std::map<string, string> tree::traverseGetCode() {
+    std::map<string, string> ret;
+    getCodeHelper(this->head, std::string(), std::string(), ret);
+    
+    return ret;
+}
+
+void tree::getCodeHelper(node* next, string currentCode, string currentSymbol, std::map<string, string> &ret) {
+    if (next != nullptr) {
+        currentCode += next->code;
+        getCodeHelper(next->left, currentCode, next->symbols, ret);
+        getCodeHelper(next->right, currentCode, next->symbols, ret);
+    } else {
+        ret[currentSymbol] = currentCode;
+    
+    }
+}
+
 // Rebuild tree
 
 node* balanceTreeHelper(std::vector<int> order, int start, int end) {
@@ -246,6 +319,48 @@ tree* balanceTree(tree unbalanced) {
     
     return newtree;
 };
+
+
+void printHuffman2DUtil(node *root, int space)
+{
+    // Base case
+    if (root == NULL)
+        return;
+ 
+    // Increase distance between levels
+    space += COUNT;
+ 
+    // Process right child first
+    printHuffman2DUtil(root->right, space);
+ 
+    // Print current node after space
+    // count
+    
+    
+    printf("\n");
+    for (int i = COUNT; i < space; i++)
+        printf(" ");
+    //printf("%d\n", root->value);
+    
+    
+    
+    printf("%c", '\'');
+    printf("%s", root->symbols.c_str());
+    printf("%s", "'(");
+    printf("%i", root->value);
+    printf("%s", "): ");
+    printf("%c\n", root->code);
+    
+ 
+    // Process left child
+    printHuffman2DUtil(root->left, space);
+}
+ 
+// Prints huffman tree
+void printHuffman2D(node *root)
+{
+    printHuffman2DUtil(root, 0);
+}
 
 // Function to print binary tree in 2D
 // It does reverse inorder traversal
@@ -279,32 +394,34 @@ void print2D(node *root)
     print2DUtil(root, 0);
 }
 
-// Example tree insertions and balancing
-void testTree(tree test)
-{
-    test.addElement(50);
-    test.addElement(100);
-    test.addElement(150);
-    test.addElement(200);
-    test.addElement(250);
-    test.addElement(75);
-    test.addElement(74);
-    test.addElement(99);
-    test.addElement(125);
-    test.addElement(175);
-    test.addElement(225);
-    test.addElement(49);
-    
-    print2D(test.head);
 
-    std::vector<int> result = test.traverse();
+// Example tree insertions and balancing
+void testTree()
+{
+    tree Tree = *new tree();
+    Tree.addElement(50);
+    Tree.addElement(100);
+    Tree.addElement(150);
+    Tree.addElement(200);
+    Tree.addElement(250);
+    Tree.addElement(75);
+    Tree.addElement(74);
+    Tree.addElement(99);
+    Tree.addElement(125);
+    Tree.addElement(175);
+    Tree.addElement(225);
+    Tree.addElement(49);
+    
+    print2D(Tree.head);
+
+    std::vector<int> result = Tree.traverse();
     std::cout << "Elements: [";
     for (auto& element : result) {
         std::cout << ' ' << element << ' ';
     }
     std::cout << "]\n";
     
-    tree balanced = *balanceTree(test);
+    tree balanced = *balanceTree(Tree);
     print2D(balanced.head);
     
     result = balanced.traverse();
@@ -315,13 +432,64 @@ void testTree(tree test)
     std::cout << "]\n";
 }
 
+void testHuffman()
+{
+    string input;
+    std::map<string, int> freqMap;
+    std::vector<node*> nodelist;
+    
+    std::cout << "Enter string: " << '\n';
+    getline(std::cin, input);
+    
+    for (auto& i : input) {
+        freqMap[string(1,i)] += 1;
+    }
+    
+    for (auto& i : freqMap) {
+        nodelist.emplace_back(new node(i.second, i.first));
+    }
+    
+    std::sort(nodelist.begin(), nodelist.end(), [](const node* left, const node* right) {
+        return left->value > right->value;
+    });
+
+    do
+    {
+        node* left = nodelist.back();
+        nodelist.pop_back();
+        node* right = nodelist.back();
+        nodelist.pop_back();
+        nodelist.insert(std::upper_bound(nodelist.begin(), nodelist.end(), left->value + right->value, [](const int &left, const node* right) { return left > right->value; }), new node(left, right));
+    } while (nodelist.size() > 1);
+
+    
+    tree Tree = *new tree(nodelist[0]);
+    
+    Tree.traverseApplyCode();
+    
+    printHuffman2D(nodelist[0]);
+    
+    std::cout << '\n';
+    std::cout << "Keys: " << '\n';
+    
+    std::map<string, string> codes = Tree.traverseGetCode();
+    
+    for (auto& i : codes) {
+        std::cout << i.first << ": " << i.second << '\n';
+    }
+    
+    std::cout << '\n';
+    std::cout << "Message: " << '\n';
+    
+    for (auto& i : input) {
+        std::cout << codes[string(1, i)];
+    }
+    std::cout << '\n';
+}
+
 
 
 int main(void) {
-    
-    tree Tree = *new tree();
-    
-    testTree(Tree);
-    
+    testHuffman();    
     return 0;
 }
